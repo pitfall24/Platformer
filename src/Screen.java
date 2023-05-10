ed2917fda8221ecd5ea7bc723ed5874dff88d14b
pitfall24 <pscovel24@gmail.com>
2023-05-10 15:13:15 -0400

    more stuff

--- src/Screen.java
+++ src/Screen.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Screen {
 	final int height;
 	final int width;
@@ -9,5 +12,29 @@ public class Screen {
 		this.width = 40;
 		this.name = name;
 		this.screen = new Tile[height][width];
+	}
+
+	public static Screen parseScreen(File screen, String name) throws Exception {
+		Scanner sc = new Scanner(screen);
+
+		{
+			String firstLine = sc.nextLine();
+			int left = firstLine.indexOf(":") + 2;
+
+			if (!name.equals(firstLine.substring(left))) {
+				sc.close();
+				throw new Exception("");
+			}
+		}
+
+		Screen out = new Screen(name);
+
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+
+			
+		}
+
+		return out;
 	}
 }
